97eb3d09dbda948bf9a48aaae603d2509abd9d11
Andres Dejesus <andresdejesus123@gmail.com>
2024-07-11 17:11:24 -0500

    learning crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use crossterm::{cursor, terminal, ExecutableCommand};
+use std::{error::Error, io, thread::sleep, time::Duration};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut stdout = io::stdout();
+
+    stdout.execute(terminal::EnterAlternateScreen)?;
+
+    sleep(Duration::from_millis(500));
+
+    stdout.execute(cursor::MoveRight(1))?;
+
+    sleep(Duration::from_millis(500));
+
+    stdout.execute(cursor::MoveRight(1))?;
+
+    sleep(Duration::from_millis(500));
+
+    stdout.execute(cursor::MoveRight(1))?;
+
+    sleep(Duration::from_millis(500));
+
+    stdout.execute(cursor::MoveTo(0, 0))?;
+
+    sleep(Duration::from_millis(500));
+
+    stdout.execute(terminal::LeaveAlternateScreen)?;
+
+    Ok(())
 }
